6620338ec5beaae8280e0f15c6271bc456623823
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-07-28 18:39:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.any? do |cheese|
-    cheese_types.include(cheese)
+    cheese_types.include?(cheese)
   end
 end
