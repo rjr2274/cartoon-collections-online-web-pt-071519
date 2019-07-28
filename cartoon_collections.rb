874a028bfa5c2920af8b8ff6c8c0c33c96816dca
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-07-28 18:39:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.include?("cheddar" || "gouda" || "camembert")
-
-
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.any? do |cheese|
+    cheese_types.include(cheese)
+  end
 end
